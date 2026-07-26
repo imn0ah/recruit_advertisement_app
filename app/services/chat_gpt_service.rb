cb5d898f0ac2fa2d495d9a34fa25641ccbba0136
naoya hino <naoya@NaoyanoMacBook-Pro.local>
2026-07-26 15:16:45 +0900

    add_ChatGPT_API

---
+++ app/services/chat_gpt_service.rb
@@ -0,0 +1,57 @@
+class ChatGptService
+    def self.generate(job_posting)
+        prompt = build_prompt(job_posting)
+
+        #APIを呼び出す
+        client = OpenAI::Client.new
+
+        response = client.chat(
+            parameters: {
+                model: "",
+                messages:[
+                    {
+                        role: "user",
+                        content: prompt
+                    }
+                ]
+            }
+        )
+
+        #chatgpt apiで作成した文章を取り出す
+        generated_text = response.dig(
+            "choices",
+            0,
+            "message",
+            "content"
+        )
+
+        generated_text
+    end
+
+
+    #chatgpt apiに渡すプロンプト作成
+    def self.build_prompt(job_posting)
+        prompt = <<~TEXT
+          以下の求人情報から求人広告を作ってください。
+
+          会社名
+          #{job_posting.company_name}
+
+          求人名
+          #{job_posting.title}
+
+          応募条件
+          #{job_posting.requirement}
+
+          求人詳細
+          #{job_posting.description}
+
+          給与
+          #{job_posting.salary}
+
+          勤務地
+          #{job_posting.salary}
+        TEXT
+    end
+end
+
